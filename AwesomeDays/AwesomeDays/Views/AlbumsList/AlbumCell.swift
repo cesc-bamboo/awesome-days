6a6de42caea593903ac6aa54605442da26abe61f
Francesc Navarro <cesc@bambooacademy.io>
2021-06-25 11:45:39 +0200

    Horizontal grid size properly set with fixed values

--- AwesomeDays/AwesomeDays/Views/AlbumsList/AlbumCell.swift
+++ AwesomeDays/AwesomeDays/Views/AlbumsList/AlbumCell.swift
@@ -12,11 +12,12 @@ struct AlbumCell: View {
     @StateObject var viewModel: AlbumCellViewModel
     private let coverHeight: CGFloat = 250
     private let gridHeight: CGFloat = 350
+    private let gridCellWidth: CGFloat = 250
     
-    @State private var isTapped: Bool = false
+    @State private var isAlbumExpanded: Bool = false
     
     var body: some View {
-        if (isTapped) {
+        if (isAlbumExpanded) {
             photosGrid(with: viewModel.assets)
         } else if let coverAsset = viewModel.coverAsset {
             coverButton(with: coverAsset)
@@ -26,11 +27,11 @@ struct AlbumCell: View {
         
         
         
-//        Button(action: onButtonTap, label: {
-//            RemoteImageView(with: asset, photosFetcher: photosFetcher)
-//                .aspectRatio(contentMode: .fit)
-//                .frame(height: isTapped ? 500 : 250)
-//        }).animation(.spring())
+        //        Button(action: onButtonTap, label: {
+        //            RemoteImageView(with: asset, photosFetcher: photosFetcher)
+        //                .aspectRatio(contentMode: .fit)
+        //                .frame(height: isTapped ? 500 : 250)
+        //        }).animation(.spring())
     }
     
     func coverButton(with asset: PHAsset) -> some View {
@@ -42,18 +43,23 @@ struct AlbumCell: View {
     }
     
     func photosGrid(with assets: [PHAsset]) -> some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: [GridItem()], spacing: 8) {
-                ForEach(assets, id: \.hash) { asset in
-                    RemoteImageView(with: asset, photosFetcher: viewModel.photosFetcher)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(height: gridHeight)
-                }
+        
+            ScrollView(.horizontal) {
+                
+                LazyHGrid(rows: [GridItem(.fixed(gridCellWidth))], spacing: 8) {
+                    
+                    ForEach(assets, id: \.hash) { asset in
+                        RemoteImageView(with: asset, photosFetcher: viewModel.photosFetcher)
+                            .aspectRatio(contentMode: .fill)
+//                            .frame(width: geometry.size.width * 0.8)
+//                            .frame(height: gridHeight)
+                    }
+                }.frame(height: gridHeight)
             }
-        }
+        
     }
     
     func onButtonTap() {
-        isTapped.toggle()
+        isAlbumExpanded.toggle()
     }
 }
