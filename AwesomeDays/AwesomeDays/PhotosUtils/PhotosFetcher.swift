757c9b282377fc0f9e15c4c5827be86835db8726
Francesc Navarro <cesc@bambooacademy.io>
2021-06-30 17:17:42 +0200

    Common behaviour extracted to a method. More optimizations.

--- AwesomeDays/AwesomeDays/PhotosUtils/PhotosFetcher.swift
+++ AwesomeDays/AwesomeDays/PhotosUtils/PhotosFetcher.swift
@@ -26,36 +26,39 @@ class PhotosFetcher: NSObject {
     }
     
     func getPermissionIfNecessary(completionHandler: @escaping (Bool) -> Void) {
-        // 1
         guard PHPhotoLibrary.authorizationStatus() != .authorized else {
             completionHandler(true)
             return
         }
-        // 2
         PHPhotoLibrary.requestAuthorization { status in
             completionHandler(status == .authorized ? true : false)
         }
     }
     
-    func fetchAssets() {// 1
+    func fetchAssets(includePhotos: Bool = true, includeSmartAlbums: Bool = false, includeCollection: Bool = false) {
         let allPhotosOptions = PHFetchOptions()
         allPhotosOptions.sortDescriptors = [
             NSSortDescriptor(
                 key: "creationDate",
                 ascending: false)
         ]
-        // 2
-        allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
-        // 3
-        smartAlbums = PHAssetCollection.fetchAssetCollections(
-            with: .smartAlbum,
-            subtype: .albumRegular,
-            options: nil)
-        // 4
-        userCollections = PHAssetCollection.fetchAssetCollections(
-            with: .album,
-            subtype: .albumRegular,
-            options: nil)
+        if includePhotos {
+            allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
+        }
+        
+        if includeSmartAlbums {
+            smartAlbums = PHAssetCollection.fetchAssetCollections(
+                with: .smartAlbum,
+                subtype: .albumRegular,
+                options: nil)
+        }
+        
+        if includeCollection {
+            userCollections = PHAssetCollection.fetchAssetCollections(
+                with: .album,
+                subtype: .albumRegular,
+                options: nil)
+        }
     }
     
     func fetchFullSizeImage(asset: PHAsset, completionHandler: @escaping (UIImage?) -> ()) {
@@ -64,19 +67,14 @@ class PhotosFetcher: NSObject {
         options.isSynchronous = false
         
         let fullImageKey = "\(asset.hash)-full"
-        if let cacheImage = fetchImageFromCache(key: fullImageKey) {
-            completionHandler(cacheImage)
-        } else {
-            requestImageAndSaveToCache(for: asset,
-                                          targetSize: PHImageManagerMaximumSize,
-                                          options: options,
-                                          cacheKey: fullImageKey,
-                                          completionHandler: completionHandler)
-        }
+        imageFromCacheOrRequest(for: asset,
+                                   targetSize: PHImageManagerMaximumSize,
+                                   options: options,
+                                   cacheKey: fullImageKey,
+                                   completionHandler: completionHandler)
     }
     
     func fetchThumbnailImage(asset: PHAsset, lowResHandler: ((UIImage?) ->())?, highResHandler: ((UIImage?) ->())?) {
-        var lowResRequest: PHImageRequestID? = nil
         let lowResImageKey = "\(asset.hash)-thumbnail-low"
         let highResImageKey = "\(asset.hash)-thumbnail-high"
         
@@ -90,40 +88,41 @@ class PhotosFetcher: NSObject {
         
         // 2. If there is no high resolution thumbnail, we'll have to fetch it. We'll fetch (or take from cache) the low resolution thumbnail to present an image quickly
         if let lowResHandler = lowResHandler {
-            if let cacheImage = fetchImageFromCache(key: lowResImageKey) {
-                lowResHandler(cacheImage)
-            } else {
-                let lowResOptions = PHImageRequestOptions()
-                lowResOptions.deliveryMode = .fastFormat
-                lowResOptions.isSynchronous = false
-                
-                lowResRequest = requestImageAndSaveToCache(for: asset,
-                                                              targetSize: CGSize(width: 512.0, height: 512.0),
-                                                              options: lowResOptions,
-                                                              cacheKey: lowResImageKey,
-                                                              completionHandler: lowResHandler)
-            }
+            let lowResOptions = PHImageRequestOptions()
+            lowResOptions.deliveryMode = .fastFormat
+            lowResOptions.isSynchronous = false
+            imageFromCacheOrRequest(for: asset,
+                                       targetSize: CGSize(width: 512.0, height: 512.0),
+                                       options: lowResOptions,
+                                       cacheKey: lowResImageKey,
+                                       completionHandler: lowResHandler)
         }
         
         if let highResHandler = highResHandler {
-            if let cacheImage = fetchImageFromCache(key: highResImageKey) {
-                highResHandler(cacheImage)
-            } else {
-                let highResOptions = PHImageRequestOptions()
-                highResOptions.deliveryMode = .highQualityFormat
-                highResOptions.isSynchronous = false
-                
-                requestImageAndSaveToCache(for: asset,
-                                              targetSize: CGSize(width: 512.0, height: 512.0),
-                                              options: highResOptions,
-                                              cacheKey: highResImageKey) { image in
-                    if let lowResRequest = lowResRequest {
-                        // If we already have the high resolution image, we cancel the low resolution request
-                        self.imageManager.cancelImageRequest(lowResRequest)
-                    }
-                    highResHandler(image)
-                }
-            }
+            let highResOptions = PHImageRequestOptions()
+            highResOptions.deliveryMode = .highQualityFormat
+            highResOptions.isSynchronous = false
+            imageFromCacheOrRequest(for: asset,
+                                       targetSize: CGSize(width: 512.0, height: 512.0),
+                                       options: highResOptions,
+                                       cacheKey: highResImageKey,
+                                       completionHandler: highResHandler)
+        }
+    }
+    
+    func imageFromCacheOrRequest(for asset: PHAsset,
+                                 targetSize: CGSize,
+                                 options: PHImageRequestOptions,
+                                 cacheKey: String,
+                                 completionHandler: @escaping (UIImage?) -> ()) {
+        if let cacheImage = fetchImageFromCache(key: cacheKey) {
+            completionHandler(cacheImage)
+        } else {
+            requestImageAndSaveToCache(for: asset,
+                                          targetSize: PHImageManagerMaximumSize,
+                                          options: options,
+                                          cacheKey: cacheKey,
+                                          completionHandler: completionHandler)
         }
     }
     
@@ -160,11 +159,9 @@ class PhotosFetcher: NSObject {
 extension PhotosFetcher: PHPhotoLibraryChangeObserver {
     func photoLibraryDidChange(_ changeInstance: PHChange) {
         DispatchQueue.main.sync {
-            // 1
             if let changeDetails = changeInstance.changeDetails(for: allPhotos) {
                 allPhotos = changeDetails.fetchResultAfterChanges
             }
-            // 2
             if let changeDetails = changeInstance.changeDetails(for: smartAlbums) {
                 smartAlbums = changeDetails.fetchResultAfterChanges
             }
